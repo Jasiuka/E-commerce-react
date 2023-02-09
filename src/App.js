fe04d3f514c806d9fea8a1b1503356193ebdbb7c
Jasiuka <luksjasiukaitis@gmail.com>
2023-02-09 20:07:57 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Home from "./routes/home/home.component";
 import SignPage from "./routes/sign-page/sign-page";
 import Navigation from "./routes/navigation/navigation.component";
 import Profile from "./routes/profile/profile.component";
-import UserSettings from "./routes/settings/settings.component";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
@@ -11,14 +10,14 @@ import Footer from "./components/footer/footer.component";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Footer />}></Route>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="sign-in" element={<SignPage />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="user-settings" element={<UserSettings />} />
-        <Route path="checkout" element={<Checkout />} />
+      <Route path="/" element={<Footer />}>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="sign-in" element={<SignPage />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
       </Route>
     </Routes>
   );
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,8 +1,92 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>This is a footer box</h1>
-    </div>
+    <Fragment>
+      <Outlet />
+      <div className="footer">
+        <div className="footer__location-box">
+          <div className="footer__location--contacts-box">
+            <h2>Contacts</h2>
+            <div className="contacts">
+              <div className="footer__location--info-box">
+                <span className="footer__location--info-label">
+                  Our adress:
+                </span>
+                <span className="footer__location--info-text">
+                  Jonavos g., 44136 Kaunas
+                </span>
+              </div>
+              <div className="footer__location--info-box">
+                <span className="footer__location--info-label">
+                  Phone number:
+                </span>
+                <span className="footer__location--info-text">+370000000</span>
+              </div>
+              <div className="footer__location--info-box">
+                <span className="footer__location--info-label">Email:</span>
+                <span className="footer__location--info-text">
+                  crwnclothing@info.com
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="footer__location--location-container">
+            <iframe
+              className="footer__location-map"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3969.898378616124!2d23.910495185646734!3d54.91492280836951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1slt!2slt!4v1675960462699!5m2!1slt!2slt"
+              loading="lazy"
+              title="location map"
+              referrerPolicy="no-referrer-when-downgrade"
+              allowFullScreen=""
+            ></iframe>
+          </div>
+        </div>
+        <div className="footer__list">
+          <div className="footer__list-navigation">
+            {/* <h3 className="footer__list-title">Navigation</h3> */}
+            <div>
+              <Link className="footer__link" to="/">
+                HOME
+              </Link>
+              <Link className="footer__link" to="shop">
+                SHOP
+              </Link>
+              <Link className="footer__link" to="profile">
+                PROFILE
+              </Link>
+              <Link className="footer__link" to="checkout">
+                CHECKOUT
+              </Link>
+            </div>
+          </div>
+          <div className="footer__list-shop-navigation">
+            {/* <h3 className="footer__list-title">Shop navigation</h3> */}
+            <div>
+              <Link className="footer__link" to="shop/hats">
+                HATS
+              </Link>
+              <Link className="footer__link" to="shop/sneakers">
+                SNEAKERS
+              </Link>
+              <Link className="footer__link" to="shop/jackets">
+                JACKETS
+              </Link>
+              <Link className="footer__link" to="shop/womens">
+                WOMENS
+              </Link>
+              <Link className="footer__link" to="shop/mens">
+                MENS
+              </Link>
+            </div>
+          </div>
+        </div>
+        <p className="copyright">
+          Copyright &copy; 2023 Crown Clothing. All rights reserved.
+        </p>
+      </div>
+    </Fragment>
   );
 };
 
