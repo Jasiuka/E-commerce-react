c18784c09fa154038df6c3e060cb8a7312074685
Jasiuka <luksjasiukaitis@gmail.com>
2023-02-20 20:03:00 +0200

    comment how to create your own redux logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,24 @@ import { compose, legacy_createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+// how to create your own logger
+/*
+const myOwnLoggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("current state: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+ */
+
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
